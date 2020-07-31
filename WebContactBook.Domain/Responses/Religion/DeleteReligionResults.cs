fc4db973027d67e35fb9b5afb1842f2c5127b7b9
an hong <hongan529@gmail.com>
2020-07-31 11:02:10 +0700

    CRUD Religion

---
+++ WebContactBook.Domain/Responses/Religion/DeleteReligionResults.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebContactBook.Domain.Responses.Religion
+{
+    public class DeleteReligionResults
+    {
+        public int ReligionId { get; set; }
+        public string Message { get; set; }
+    }
+}
